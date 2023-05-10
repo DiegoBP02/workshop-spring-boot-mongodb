727c09a09be4dc9c8974931fc0355ff9d2f76440
Diego Batista Pereira <diegobpcelular@gmail.com>
2023-05-10 17:18:34 -0300

    consulta com varios criterios

--- src/main/java/cursoJava/workshopmongo/services/PostService.java
+++ src/main/java/cursoJava/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import cursoJava.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 1000 * 60 * 60 * 24);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 
 }
